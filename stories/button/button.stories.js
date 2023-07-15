19b48a7b6f6160fe272e0230b5ddd1dc80a6e20a
pablopsdigital <pablopsdigital@gmail.com>
2023-07-15 11:28:28 +0200

    doc(button): Include argTypes in button stories

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -2,6 +2,28 @@ import { createButton } from "./button";
 
 export default {
   title: "HTML Design System/Atoms/Button",
+  argTypes: {
+    label: {
+      name: "label",
+      control: {
+        type: "text",
+      },
+    },
+    style: {
+      name: "style",
+      options: ["filled", "outlined"],
+      control: {
+        type: "radio",
+      },
+    },
+    size: {
+      name: "size",
+      options: ["small", "medium", "large"],
+      control: {
+        type: "radio",
+      },
+    },
+  },
 };
 
 const Template = ({ label, ...args }) => {
